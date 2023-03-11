2ef9ef822e9429d7a721a0604c9516bc1adcad3f
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-03-12 00:02:52 +0200

    chore: Camera & Location

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,36 @@
 <template>
   <q-page class="constrain-more 1-pa-md">
     <div class="camera-frame q-pa-md">
-      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240px"
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" size="lg" icon="eva-camera" rounded />
+      <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
+        round
+        color="grey-10"
+        size="lg"
+        icon="eva-camera"
+        rounded
+      />
+      <q-file
+        v-else
+        outlined
+        v-model="imageUpload"
+        @input="captureImageFallBack"
+        label="Selecione uma imagem"
+        accept="image/*"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
       <div class="row justify-center q-ma-md">
         <q-input
           class="col col col-sm-6"
@@ -19,14 +45,22 @@
           v-model="post.location"
           label="Localização"
           dense
+          :loading="locationLoading"
         >
           <template v-slot:append>
-            <q-btn round dense flat icon="eva-navigation-2-outline" />
+            <q-btn
+              v-if="!locationLoading && locationSupported"
+              @click="getLocation"
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline"
+            />
           </template>
         </q-input>
       </div>
       <div class="row justify-center q-mt-lg">
-        <q-btn unelevated rounded color="primary" label="Publicar" />
+        <q-btn unelevated rounded color="grey-10" label="Publicar" />
       </div>
     </div>
   </q-page>
@@ -34,7 +68,9 @@
 
 <script>
 import { uid } from "quasar";
+require("md-gum-polyfill");
 
+// ... code using getUserMedia...
 export default {
   name: "PageCamera",
   data() {
@@ -46,7 +82,134 @@ export default {
         photo: null,
         date: Date.now(),
       },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+      locationLoading: false,
     };
+  },
+  computed: {
+    locationSupported() {
+      if ("geolocation" in navigator) return true;
+      return false;
+    },
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream;
+        })
+        .catch((error) => {
+          this.hasCameraSupport = false;
+        });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    captureImageFallBack(file) {
+      this.post.photo = file;
+
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
+
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        var img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach((track) => {
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    },
+    getLocation() {
+      this.locationLoading = true;
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.getCityAndCountry(position);
+        },
+        (err) => {
+          console.log("err: ", err);
+        },
+        { timeout: 10 }
+      );
+    },
+    getCityAndCountry(position) {
+      let apiUrl = `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`;
+      this.$axios
+        .get(apiUrl)
+        .then((result) => {
+          this.locationSuccess(result);
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+        });
+    },
+    locationSuccess(result) {
+      this.post.location = result.data.city;
+      if (result.data.country) {
+        this.post.location += `, ${result.data.country}`;
+      }
+      this.locationLoading = false;
+    },
+    locationError() {
+      this.$q.dialog({
+        title: "Erro",
+        message: "Não foi possível determinar a sua localização!",
+      });
+      this.locationLoading = false;
+    },
+  },
+  mounted() {
+    this.initCamera();
+  },
+  beforeDestroy() {
+    if (this.hasCameraSupport) {
+      this.disableCamera();
+    }
   },
 };
 </script>
