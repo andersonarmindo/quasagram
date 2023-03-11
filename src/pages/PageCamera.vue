f456781d4d561d1a76aefcbfd4c9e8debe7b22cc
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-03-11 13:39:56 +0200

    chore:  Design Home & Camera Pages

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,58 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera page</h5>
+  <q-page class="constrain-more 1-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="grey-10" size="lg" icon="eva-camera" rounded />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          class="col col col-sm-6"
+          v-model="post.caption"
+          label="Legenda"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          class="col col-sm-6"
+          v-model="post.location"
+          label="Localização"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Publicar" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { uid } from "quasar";
+
 export default {
   name: "PageCamera",
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: "",
+        location: "",
+        photo: null,
+        date: Date.now(),
+      },
+    };
+  },
 };
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,103 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home page</h5>
+  <q-page class="constrain 1-pa-md">
+    <div class="row row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="../../public/img/anderson.jpg" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">_andersonarmindo</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+
+          <q-card-section>
+            <div class="text-h6">{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="../../public/img/anderson.jpg" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">_andersonarmindo</q-item-label>
+            <q-item-label caption> Anderson Armindo </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { date } from "quasar";
 export default {
   name: "PageHome",
+
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+          date: 1678519464786,
+          location: "Maputo, Mozambique",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 2,
+          caption: "Golden Gate Bridge",
+          date: 1678519464786,
+          location: "Maputo, Mozambique",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Gate Bridge",
+          date: 1678519464786,
+          location: "Maputo, Mozambique",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 4,
+          caption: "Golden Gate Bridge",
+          date: 1678519464786,
+          location: "Maputo, Mozambique",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+      ],
+    };
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, "D MMMM HH:mm");
+    },
+  },
 };
 </script>
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>
