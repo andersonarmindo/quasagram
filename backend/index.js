74845514913de1ac16e20ac52619f0eebb8c795a
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-03-12 09:24:57 +0200

    chore: Backend with Firebase, Node.js & Express

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+/*
+Dependencies
+*/
+
+const express = require("express");
+
+/*
+Config-express
+*/
+const app = express();
+
+/*
+Endpoint - posts
+*/
+app.get("/posts", (request, response) => {
+  let posts = [
+    {
+      caption: "Golden Gate Bridge",
+      location: "Maputo, Mozambique",
+    },
+    {
+      caption: "London Eye",
+      location: "London",
+    },
+  ];
+  response.send(posts);
+});
+
+/*
+Listen
+*/
+
+app.listen(3000);
