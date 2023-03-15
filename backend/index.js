7b4d7cd86bdd0426fcceaff0d551e130b9c75568
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-03-15 10:45:57 +0200

    Backend sync

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ Dependencies
 */
 
 const express = require("express");
+const admin = require("firebase-admin");
 
 /*
 Config-express
@@ -10,19 +11,33 @@ Config-express
 const app = express();
 
 /*
+Config-firebase
+*/
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+/*
 Endpoint - posts
 */
 app.get("/posts", (request, response) => {
-  let posts = [
-    {
-      caption: "Golden Gate Bridge",
-      location: "Maputo, Mozambique",
-    },
-    {
-      caption: "London Eye",
-      location: "London",
-    },
-  ];
+  setTimeout;
+  response.set("Access-Control-Allow-Origin", "*");
+  let posts = [];
+  loadingPosts: false;
+  db.collection("posts")
+    .orderBy("date", "desc")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        posts.push(doc.data());
+      });
+    });
+
   response.send(posts);
 });
 
